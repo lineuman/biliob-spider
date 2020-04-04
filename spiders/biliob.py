7b8970a809b83746ebcb2532319028c8d91e1ce1
jannchie <jannchie@gmail.com>
2020-04-05 03:00:01 +0900

    FEATURE: add interval

--- spiders/biliob.py
+++ spiders/biliob.py
@@ -13,7 +13,7 @@ from utils import sub_channel_2_channel
 
 class BiliobSpider(Spider):
 
-  def __init__(self, name, thread=1, interval=0.05):
+  def __init__(self, name, thread=1, interval=0.1):
     super().__init__()
     self.name = name
     self.db = db

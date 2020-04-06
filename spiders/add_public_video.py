6a0597f30a999bd09f19e4a36b141ca412599306
jannchie <jannchie@gmail.com>
2020-04-06 12:54:31 +0900

    UPDATE: Auto add video

--- spiders/add_public_video.py
+++ spiders/add_public_video.py
@@ -1,21 +1,26 @@
-from utils import enc
+from utils import enc, dec
 from biliob import BiliobSpider
 from time import sleep
 from datetime import datetime
 from datetime import timedelta
 url = 'https://space.bilibili.com/ajax/member/getSubmitVideos?mid={}&pagesize=10&page=1&order=pubdate'
+url = 'https://api.bilibili.com/x/space/arc/search?mid={}&ps=100&tid=0&pn=1&keyword=&order=pubdate'
 
 
 class AddPublicVideoSpider(BiliobSpider):
   def __init__(self):
-    super().__init__(name='追加作者最新上传的视频', interval=0.5)
+    super().__init__(name='追加作者最新上传的视频')
 
   def gen_url(self):
     while True:
       try:
         cursor = self.db.author.find({}, {'mid': 1}).batch_size(20)
-        for each_author in cursor:
-          yield url.format(each_author['mid'])
+        try:
+          for each_author in cursor:
+            yield url.format(each_author['mid'])
+        except Exception as e:
+          self.logger.exception(e)
+          sleep(1)
       except Exception as e:
         self.logger.exception(e)
         sleep(1)
@@ -23,29 +28,42 @@ class AddPublicVideoSpider(BiliobSpider):
   def parse(self, res):
     try:
       j = res.json()
-      aid_list = []
-      if 'data' not in j or 'vlist' not in j['data']:
+      result = []
+      if 'data' not in j or 'list' not in j['data'] or 'vlist' not in j['data']['list']:
         return None
-      for each_video in j['data']['vlist']:
-        aid_list.append(each_video['aid'])
-      return aid_list
+      bvid = None
+      aid = None
+      for each_video in j['data']['list']['vlist']:
+        if 'bvid' in each_video:
+          bvid = each_video['bvid']
+        if 'aid' in each_video:
+          aid = each_video['aid']
+        result.append([bvid, aid])
+      return result
     except Exception as e:
-      self.logger(e)
+      self.logger.exception(e)
 
-  def update_video_interval(self, interval: int, aid):
+  def update_video_interval(self, interval: int, aid, bvid):
     now = datetime.utcnow() + timedelta(hours=8)
+    if aid == None:
+      aid = enc(bvid)
+    if bvid == None:
+      bvid = dec(aid)
     return {
         'next': now,
-        'date': now,
         'interval': interval,
         'aid': aid,
-        'bvid': enc(aid)[2:]
+        'bvid': bvid
     }
 
   def save(self, items):
-    for aid in items:
-      self.db.video_interval.update(
-          {'aid': aid}, self.update_video_interval(3600, aid), upsert=True)
+    for [bvid, aid] in items:
+      bvid = bvid.lstrip('BV')
+      interval_data = self.db.video_interval.find_one(
+          {'bvid': bvid, 'aid': aid})
+      if interval_data == None:
+        self.db.video_interval.update_one(
+            {'bvid': bvid, 'aid': aid}, {'$set': self.update_video_interval(3600 * 6, aid, bvid), '$setOnInsert': {'date': datetime.utcnow() + timedelta(hours=8)}}, upsert=True)
 
     return items
 
